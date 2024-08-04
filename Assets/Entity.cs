e52b15909ed8603ecf909c3feb40efece2258e78
Bish0pdev <62082484+Bish0pdev@users.noreply.github.com>
2024-08-04 07:34:06 -0500

    Added Connections

--- Assets/Entity.cs
+++ Assets/Entity.cs
@@ -1,19 +1,59 @@
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 
+[System.Serializable]
+[RequireComponent(typeof(SpriteRenderer))]
 public class Entity : MonoBehaviour
 {
+    private SpriteRenderer _sr;
+
     public float radius = 1;
     public List<E_Connection> connections = new List<E_Connection>();
 
-    public static void Create_New(EntitySpawner spawner, Vector2 position, float radius) {
+    public void On_Spawn(EntitySpawner spawner) {
+        _sr = GetComponent<SpriteRenderer>();
+        foreach(E_Connection connection in connections) {
+            LineRenderer lr = new GameObject().AddComponent<LineRenderer>();
+            lr.transform.parent = transform;
+            lr.positionCount = 2;
+            lr.SetPosition(0,connection.connection_one.transform.position);
+            lr.SetPosition(1,connection.connection_two.transform.position);
+            lr.widthMultiplier = spawner.connection_Width;
+            lr.material = spawner.connection_Mat;
+        }
+
+        _sr.color = spawner.entity_Color;
+    }
+
+    public static Entity Create_New(EntitySpawner spawner, Vector2 position, float radius) {
         Entity newEntity = Instantiate(spawner.entity_Prefab).GetComponent<Entity>();
         newEntity.transform.position = position;
         newEntity.transform.localScale = new Vector3(1 * radius,1 * radius,0.1f);
+
+        if(spawner.lastSpawnedEntity != null && spawner.lastSpawnedEntity != newEntity) {
+            Connect(spawner.lastSpawnedEntity,newEntity);
+        }
+
+        newEntity.On_Spawn(spawner);
+        return newEntity;
         
+    }
+    public static E_Connection Connect(Entity one, Entity two) {
+        E_Connection newConnection = new E_Connection
+        {
+            connection_one = one,
+            connection_two = two
+        };
+
+        one.connections.Add(newConnection);
+        two.connections.Add(newConnection);
+
+        return newConnection;
     }
 }
 
+[System.Serializable]
 public struct E_Connection {
     public Entity connection_one;
     public Entity connection_two;
--- Assets/EntitySpawner.cs
+++ Assets/EntitySpawner.cs
@@ -6,15 +6,33 @@ public class EntitySpawner : MonoBehaviour
 {
     public GameObject entity_Prefab;
 
-    public Collider2D worldBounds;
+    public Vector2 worldBoundSize;
+
+    public Color entity_Color;
+
+    public float connection_Width = 0.2f;
+    public Material connection_Mat;
 
     public int start_amount = 10;
 
-    void Start()
+    public AnimationCurve size_Probability_Curve;
+
+    public Entity lastSpawnedEntity { get; private set; }
+
+    void Awake()
     {
         for(int i = 0; i <= start_amount; i++) {
-            
+            Vector3 newPos = new Vector3(Random.Range(-worldBoundSize.x, worldBoundSize.x),Random.Range(-worldBoundSize.y,worldBoundSize.y));
+
+            float size = size_Probability_Curve.Evaluate(Random.value * size_Probability_Curve[size_Probability_Curve.length-1].value);
+
+            lastSpawnedEntity = Entity.Create_New(this,newPos, size);
         }
 
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position,new Vector3(worldBoundSize.x*2,worldBoundSize.y*2, 0.1f));
     }
 }
